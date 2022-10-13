1df9a0586be919c067d7affd332bd43559ece8a8
mubinui <mubin41@student.sust.edu>
2022-10-13 17:48:44 +0600

    Jackson dependency added

--- src/main/java/org/acme/MovieResource.java
+++ src/main/java/org/acme/MovieResource.java
@@ -9,42 +9,50 @@ import java.util.stream.Collectors;
 
 @Path("/movies")
 public class MovieResource{
-    public static List<String> movieList = new ArrayList<>();
+    public static List<Movie> movieList = new ArrayList<>();
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.APPLICATION_JSON)
     public Response getMovies(){
         return Response.ok(movieList).build();
     }
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.APPLICATION_JSON)
     @Path("/size")
     public Integer countMovies(){
         return movieList.size();
     }
     // Post method for adding new items to the list
     @POST
-    @Produces(MediaType.TEXT_PLAIN)
-    @Consumes(MediaType.TEXT_PLAIN)
-    public Response createMovie(String newMovies){
-        movieList.add(newMovies);
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createMovie(Movie movie){
+        movieList.add(movie);
         return Response.ok(movieList).build();
 
     }
-    @PUT
-    @Path("{movieToUpdate}")
-    @Produces(MediaType.TEXT_PLAIN)
+//    @PUT
+//    @Path("{movieToUpdate}")
+//    @Produces(MediaType.TEXT_PLAIN)
+//    @Consumes(MediaType.TEXT_PLAIN)
+//    public Response updateMovie (@PathParam("movieToUpdate") String movieToUpdate, @QueryParam("movie") String updateMovie){
+//        movieList.stream().map(movie->
+//        {
+//            if (movie.equals(movieToUpdate)) return movieToUpdate;
+//            else return movie;
+//
+//
+//        }).collect(Collectors.toList());
+//        return Response.ok(movieList).build();
+//    }
+    @DELETE
+    @Path("{movieToDelete}")
     @Consumes(MediaType.TEXT_PLAIN)
-    public Response updateMovie (@PathParam("movieToUpdate") String movieToUpdate, @QueryParam("movie") String updateMovie){
-        movieList.stream().map(movie->
-        {
-            if (movie.equals(movieToUpdate)) return movieToUpdate;
-            else return movie;
-
-
-        }).collect(Collectors.toList());
-        return Response.ok(movieList).build();
+    public Response deleteMovie(@PathParam("movieToDelete") String movieToDelete){
+        boolean removed = movieList.remove(movieToDelete);
+        return removed ? Response.noContent().build():Response.status(Response.Status.BAD_REQUEST).build();
     }
+
 
 
 }
