162f0e4bb3ce66831df209a1a757063a0ad58b72
mubinui <mubin41@student.sust.edu>
2022-10-13 15:12:20 +0600

    Get Post Put Produces Consumes

---
+++ src/main/java/org/acme/MovieResource.java
@@ -0,0 +1,50 @@
+package org.acme;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Path("/movies")
+public class MovieResource{
+    public static List<String> movieList = new ArrayList<>();
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response getMovies(){
+        return Response.ok(movieList).build();
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/size")
+    public Integer countMovies(){
+        return movieList.size();
+    }
+    // Post method for adding new items to the list
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.TEXT_PLAIN)
+    public Response createMovie(String newMovies){
+        movieList.add(newMovies);
+        return Response.ok(movieList).build();
+
+    }
+    @PUT
+    @Path("{movieToUpdate}")
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.TEXT_PLAIN)
+    public Response updateMovie (@PathParam("{movieToUpdate}") String movieToUpdate, @QueryParam("movie") String updateMovie){
+        movieList.stream().map(movie->
+        {
+            if (movie.equals(movieToUpdate)) return movieToUpdate;
+            else return movie;
+
+
+        }).collect(Collectors.toList());
+        return Response.ok(movieList).build();
+    }
+
+
+}
