b8a017d002d255f0977115877184ed9eeab6c1df
mubinui <mubin41@student.sust.edu>
2022-10-17 07:29:39 +0600

    Jackson dependency added

--- src/main/java/org/acme/MovieResource.java
+++ src/main/java/org/acme/MovieResource.java
@@ -5,6 +5,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Path("/movies")
@@ -31,28 +32,34 @@ public class MovieResource{
         return Response.ok(movieList).build();
 
     }
-//    @PUT
-//    @Path("{movieToUpdate}")
-//    @Produces(MediaType.TEXT_PLAIN)
-//    @Consumes(MediaType.TEXT_PLAIN)
-//    public Response updateMovie (@PathParam("movieToUpdate") String movieToUpdate, @QueryParam("movie") String updateMovie){
-//        movieList.stream().map(movie->
-//        {
-//            if (movie.equals(movieToUpdate)) return movieToUpdate;
-//            else return movie;
-//
-//
-//        }).collect(Collectors.toList());
-//        return Response.ok(movieList).build();
-//    }
-    @DELETE
-    @Path("{movieToDelete}")
+    @PUT
+    @Path("{id}/{title}")
+    @Produces(MediaType.TEXT_PLAIN)
     @Consumes(MediaType.TEXT_PLAIN)
-    public Response deleteMovie(@PathParam("movieToDelete") String movieToDelete){
-        boolean removed = movieList.remove(movieToDelete);
-        return removed ? Response.noContent().build():Response.status(Response.Status.BAD_REQUEST).build();
-    }
+    public Response updateMovie (@PathParam("id") Long id, @PathParam("title") String title){
 
+        movieList.stream().map(movie->
+        {
+            if (movie.getId()==id){
+                movie.setTitle(title);
+                return movie;
+            }
+            else return movie;
+
+        }).collect(Collectors.toList());
+        return Response.ok(movieList).build();
+    }
+    @DELETE
+    @Path("{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response deleteMovie(@PathParam("id") Long id ){
+        Optional<Movie> movieToDelete = movieList.stream().filter(movie -> movie.getId().equals(id)).findFirst();
+        boolean removed = false;
+        if(movieToDelete.isPresent()) removed = movieList.remove(movieList.remove(movieToDelete));
+        if (removed) return Response.noContent().build(); // Suppose to happen
+        return Response.status(Response.Status.BAD_REQUEST).build();
+
+    }
 
 
 }
